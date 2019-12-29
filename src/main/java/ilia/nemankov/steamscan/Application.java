3a529baf18de9d9b64f817a2e451b7a79026c100
Ilia Nemankov <ilia.nemankov@gmail.com>
2019-12-29 14:01:52 +0200

    Init commit

---
+++ src/main/java/ilia/nemankov/steamscan/Application.java
@@ -0,0 +1,46 @@
+package ilia.nemankov.steamscan;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class Application{
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+    @Value("${spring.datasource.username}")
+    private String databaseUsername;
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClass;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(databaseUsername);
+        dataSource.setPassword(databasePassword);
+        dataSource.setDriverClassName(driverClass);
+        return dataSource;
+    }
+
+    @Bean(name = "liquibase")
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/liquibase/db-changelog-master.xml");
+        liquibase.setDataSource(getDataSource());
+        return liquibase;
+    }
+
+}
