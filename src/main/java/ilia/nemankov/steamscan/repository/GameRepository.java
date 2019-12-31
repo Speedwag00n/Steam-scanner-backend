56fa7c5d1a10a835938d0a89be15feccb7cd758a
Ilia Nemankov <ilia.nemankov@gmail.com>
2019-12-31 17:15:13 +0200

    Add changeset for game table, game entity and game repository

---
+++ src/main/java/ilia/nemankov/steamscan/repository/GameRepository.java
@@ -0,0 +1,10 @@
+package ilia.nemankov.steamscan.repository;
+
+import ilia.nemankov.steamscan.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
